59bc370a889d66bd05804142b46efca24ca5368c
lewislin3 <lewislin3@gmail.com>
2017-12-25 21:18:22 +0800

    GUI update

---
+++ IoT_webGUI.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen
+import tkinter as tk
+import json
+import time
+
+
+
+win=tk.Tk()
+win.title("I want to skip the class")
+
+label=tk.Label(win, text="Please key in the reigon")#建立標籤物件
+label.pack()#顯示元件
+
+e=tk.Entry(win)
+e.pack()
+
+def insert_point():
+    var=e.get()
+b1=tk.Button(win,text='OK',command=insert_point)
+b1.pack()
+
+t = tk.Text(win,height=2)
+t.pack()
+
+
+
+
+win.mainloop()
+
